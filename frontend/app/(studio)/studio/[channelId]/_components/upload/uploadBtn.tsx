6c9df0e4e82fba63ce195c25ad88a6ba901dbfa0
Ashutosh <clashutosh04@gmail.com>
2026-05-25 11:42:50 +0530

    fix: add 'use client' directive to UploadBtn

--- frontend/app/(studio)/studio/[channelId]/_components/upload/uploadBtn.tsx
+++ frontend/app/(studio)/studio/[channelId]/_components/upload/uploadBtn.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from '@/components/ui/button'
 import { IconVideoPlus } from '@tabler/icons-react'
 import { useUpload } from "@/components/providers/upload-provider";
